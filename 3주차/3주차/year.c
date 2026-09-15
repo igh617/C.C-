1defe0a69a6e32ad80e4d02e88ae019f825f0cc6
ligthyqu <igwang6432@gmail.com>
2026-09-15 16:07:20 +0900

    Add files via upload

---
+++ 3주차/3주차/year.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+int year;
+
+printf("년도를 입력하세요");
+scanf("%d",&year);
+
+if((year % 4 ==0 && year % 100 !=0) || year % 400 ==0){
+   printf("윤년입니다");
+
+}
+else {
+   printf("평년입니다.");
+}
+
+   return 0;
+}
